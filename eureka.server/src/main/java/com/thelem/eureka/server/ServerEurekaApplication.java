9081991183de838604a914019712465b2aeb1980
Souquere <Adrien.SOUQUERE@thelem-assurances.fr>
2020-07-31 16:14:27 +0200

    clean maven package

--- eureka.server/src/main/java/com/thelem/eureka/server/ServerEurekaApplication.java
+++ eureka.server/src/main/java/com/thelem/eureka/server/ServerEurekaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//@EnableAdminServer
 public class ServerEurekaApplication {
 
 	public static void main(String[] args) {
--- oauth2.server/src/main/java/com/thelem/microservice/oauth2/server/config/OAuth2Config.java
+++ oauth2.server/src/main/java/com/thelem/microservice/oauth2/server/config/OAuth2Config.java
@@ -1,7 +1,23 @@
 package com.thelem.microservice.oauth2.server.config;
 
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
 
+public class OAuth2Config extends GlobalAuthenticationConfigurerAdapter{
+
+	 @Override
+	    public void init(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.inMemoryAuthentication()
+	                .withUser("thelem")
+	                .password(passwordEncoder().encode("thelem"))
+	                .roles("ADMIN");
+	    }
+	 
+	    @Bean
+	    public BCryptPasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 }
